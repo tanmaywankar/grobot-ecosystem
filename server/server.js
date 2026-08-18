b573ce3be24b7752510210296e56ff41e660d148
Tanmay Wankar <62742498+tanmaywankar@users.noreply.github.com>
2026-08-18 18:02:25 +0530

    data validation using zod

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
-import {z} from "zod";
+import { z } from "zod";
 const app = express();
 const PORT = process.env.PORT || 5000;
 
@@ -11,18 +11,17 @@ app.use(cors());
 app.use(express.json());
 
 const TelemetrySchema = z.object({
-temperature: z.number(),
-humidity: z.number(),
-light: z.number(),
-soilMoisture: z.number().min(0).max(100),
-rawAdc: z.number().int().optional(),
+  temperature: z.number(),
+  humidity: z.number(),
+  light: z.number(),
+  soilMoisture: z.number().min(0).max(100),
+  rawAdc: z.number().int().optional(),
 });
 
 app.post("/api/telemetry", (req, res) => {
-    
   const clientKey = req.headers["grobot-key"];
 
-// To check if the data being received is valid
+  // To check if the data being received is valid
   if (!clientKey || clientKey != HARDWARE_API_KEY) {
     console.log("Error: Invalid or missing API keys");
     return res.status(401).json({
@@ -30,13 +29,28 @@ app.post("/api/telemetry", (req, res) => {
       error: "Invalid or missing key",
     });
   }
-  console.log("Access granted");
+
+  const validation = TelemetrySchema.safeParse(req.body);
+
+  if (!validation.success) {
+    console.log("validation error", validation.error.format());
+    return res.status(400).json({
+      success: false,
+      issue: validation.error.issues,
+      error: "Uncomplete or invalid info",
+    });
+  }
+
+  const parsedData = {
+    ...validation.data,
+    receivedAt: new Date().toISOString(),
+  };
+
   return res.status(200).json({
     success: true,
-    message: "auth success",
+    message: "validation successfull",
+    data: parsedData,
   });
-
-
 });
 
 app.listen(PORT, () => {
